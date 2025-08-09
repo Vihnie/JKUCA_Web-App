95a2b71efffa1fdb0273b1518cfc6cd6bb183833
Sirmakau24 <brianm9681@gmail.com>
2025-08-09 21:59:30 +0300

    initial 7

--- client/src/pages/ResourcesPage.js
+++ client/src/pages/ResourcesPage.js
@@ -1,13 +1,13 @@
 export default function ResourcesPage() {
   return (
     <div className="page-container">
-      <h1>Resources & Downloads</h1>
-      <ul>
-        <li><a href="/docs/constitution.pdf">JKUCA Constitution</a></li>
-        <li><a href="/docs/event-proposal.pdf">Event Proposal Form</a></li>
-        <li><a href="/docs/sample-letter.pdf">Sample Letter to Companies</a></li>
-        <li><a href="/docs/meeting-minutes.zip">Meeting Minutes Archive</a></li>
-      </ul>
-    </div>
+  <h1>Resources & Downloads</h1>
+  <ul>
+    <li><a href="/docs/constitution.pdf" className="download-btn" download>JKUCA Constitution</a></li>
+    <li><a href="/docs/event-proposal.pdf" className="download-btn" download>Event Proposal Form</a></li>
+    <li><a href="/docs/sample-letter.pdf" className="download-btn" download>Sample Letter to Companies</a></li>
+    <li><a href="/docs/meeting-minutes.zip" className="download-btn" download>Meeting Minutes Archive</a></li>
+  </ul>
+</div>
   );
 }
