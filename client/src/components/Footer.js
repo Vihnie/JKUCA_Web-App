8b6ddf996373163b0971a227f34139f61467ba82
Sirmakau24 <brianm9681@gmail.com>
2025-08-09 14:41:51 +0300

    initial 3

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} JKUCA - All Rights Reserved</p>
-      <p>Email: jkucastudentsunion@gmail.com | Phone: +2547XXXXXXXX</p>
+      <p>Email: jkucastudentsunion@gmail.com | Phone: +254726002906</p>
     </footer>
   );
 }
